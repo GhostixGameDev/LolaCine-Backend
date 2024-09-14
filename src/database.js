9d46d9af8e6307da14b87e9b191ea4f526f52b9c
Haziel <business@ghostix.network>
2024-09-14 18:17:35 -0300

    temp deploy

--- src/database.js
+++ src/database.js
@@ -11,6 +11,7 @@ db.connect(function(error) {
   console.log("Connected!");
   // Create tables if not exist
   const setupQueries = [
+    `DROP TABLE votos`,
     `CREATE TABLE IF NOT EXISTS \`votos\` (
       ID tinyint AUTO_INCREMENT PRIMARY KEY,
       school tinytext NOT NULL,
